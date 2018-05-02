bed236cee7ad8aa8714b5e94ba12fb998cec0865
Anurag Patil <aspanurag@gmail.com>
2018-05-02 12:08:13 -0400

    Basic Routing | Active CSS | Button Routes Programmable Routes

--- routing/src/app/servers/servers.component.ts
+++ routing/src/app/servers/servers.component.ts
@@ -1,18 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { ServersService } from './servers.service';
+import { Component, OnInit } from "@angular/core";
+import { ServersService } from "./servers.service";
+import { Router, ActivatedRoute } from "@angular/router";
 
 @Component({
-  selector: 'app-servers',
-  templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  selector: "app-servers",
+  templateUrl: "./servers.component.html",
+  styleUrls: ["./servers.component.css"]
 })
 export class ServersComponent implements OnInit {
-  private servers: {id: number, name: string, status: string}[] = [];
+  private servers: { id: number; name: string; status: string }[] = [];
 
-  constructor(private serversService: ServersService) { }
+  constructor(
+    private serversService: ServersService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit() {
     this.servers = this.serversService.getServers();
   }
 
+  onReload() {
+    //Here relative path will not produce error because navigate doesn't know on which route it is.
+    // this.router.navigate(["servers"]);
+
+    //To make this relative. hence it will give error now.  i.e. it will go  for localhost:4200/servers/servers
+    // this.router.navigate(["servers"], { relativeTo: this.route });
+  }
 }
