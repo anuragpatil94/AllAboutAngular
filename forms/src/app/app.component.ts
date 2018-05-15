13122cb147422ab6b49dc80eed00f615c88152d3
Anurag Patil <aspanurag@gmail.com>
2018-05-15 14:14:31 -0400

    Template Driven | Form Submit | Create NgForm

--- forms/src/app/app.component.ts
+++ forms/src/app/app.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { NgForm } from "@angular/forms";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"]
 })
 export class AppComponent {
   suggestUserName() {
-    const suggestedName = 'Superuser';
+    const suggestedName = "Superuser";
+  }
+
+  onSubmit(form: NgForm) {
+    console.log(form);
   }
 }
